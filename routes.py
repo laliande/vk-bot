dae5493de40dcc71f741443f8910e8abc94846e2
Daniella Starchenko <laliande7@gmail.com>
2020-07-01 00:19:44 +0300

    created route get data about group

--- routes.py
+++ routes.py
@@ -1,8 +1,24 @@
 from flask import Blueprint
+from flask import request
+from flask import Response
+from json import dumps, dump
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/')
-def index():
-    return 'hello world'
+@api.route('/getDataAboutGroup', methods=['POST'])
+def getDataAboutGroup():
+    data = request.get_json()
+    if data is None:
+        return Response(dumps({'Response': 'no valid mimetype data'}), status=404, mimetype='application/json')
+    else:
+        try:
+            about_group = {'token': data['token'], 'group_id': data['group_id'],
+                           'version_api': data['version_api']}
+
+            with open('data.json', 'w') as f:
+                dump(about_group, f)
+
+            return Response(dumps({'Response': 'ok'}), status=200, mimetype='application/json')
+        except:
+            return Response(dumps({'Response': 'no valid format data'}), status=404, mimetype='application/json')
